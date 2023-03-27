1cbe46b425750d67463ae953bd6307ad457c52e4
arifbp666 <arifbudianto62@yahoo.co.id>
2023-03-28 02:15:44 +0700

    Tugas rev

--- HW3.js
+++ HW3.js
@@ -5,111 +5,115 @@ let angkaAcak = [];
 for (let i = 0; i < 100; i++) {
   angkaAcak.push(Math.floor(Math.random() * 50) + 1);
 }
-console.log(angkaAcak);
+console.log("angka acak:", angkaAcak);
 
 // Array untuk angka genap dan angka ganjil
 let genap = [];
 let ganjil = [];
 
-// Dibagi menjadi 2 array terpisah
+// Dibagi menjadi 2 array terpisah berdasarkan index
 for (let i = 0; i < angkaAcak.length; i++) {
-  if (angkaAcak[i] % 2 == 0) {
+  if (i % 2 == 0) {
     genap.push(angkaAcak[i]);
   } else {
     ganjil.push(angkaAcak[i]);
   }
 }
-console.log(genap);
-console.log(ganjil);
+console.log("angka genap:", genap);
+console.log("angka ganjil:", ganjil);
 
-// Untuk mencari nilai minimal dan maksimal
-let minGenap = Math.min(...genap);
-let maxGenap = Math.max(...genap);
-let minGanjil = Math.min(...ganjil);
-let maxGanjil = Math.max(...ganjil);
-
-console.log("Minimum angka genap", minGenap);
-console.log("Maksimum angka genap", maxGenap);
-console.log("Minimum angka ganjil", minGanjil);
-console.log("Maksimum angka ganjil", maxGanjil);
-
-// Menghitung jumlah genap
-let totalGenap = 0;
-for (let i = 0; i < genap.length; i++) {
-  totalGenap += genap[i];
+// Untuk mencari nilai minimal dan maksimal dengan function
+function findMax(angkaAcak) {
+  let max = angkaAcak[0]
+  for (let i = 0; i < angkaAcak.length; i++) {
+    if (max < angkaAcak[i]) {
+      max = angkaAcak[i]
+    }
+  }
+  return max
 }
-console.log("Jumlah genap", totalGenap);
 
-// Menghitung jumlah ganjil
-let totalGanjil = 0;
-for (let i = 0; i < ganjil.length; i++) {
-  totalGanjil += ganjil[i];
+function findMin(angkaAcak) {
+  let min = angkaAcak[0]
+  for (let i = 0; i < angkaAcak.length; i++) {
+    if (min > angkaAcak[i]) {
+      min = angkaAcak[i]
+    }
+  }
+  return min
 }
-console.log("Jumlah ganjil", totalGanjil);
 
-// Total Genap dan Ganjil
-total = totalGenap + totalGanjil;
-console.log("Total keduanya", total);
+maxGenap = findMax(genap)
+minGenap = findMin(genap)
+console.log("Max genap", maxGenap)
+console.log("Min genap:", minGenap)
+
+maxGanjil = findMax(ganjil)
+minGanjil = findMin(ganjil)
+console.log("Max ganjil:", maxGanjil)
+console.log("Min ganjil:", minGanjil)
+
+
+// Menghitung total dengan function
+function total(angkaAcak) {
+  let total = 0
+  for (let i = 0; i < angkaAcak.length; i++) {
+    total += angkaAcak[i]
+  }
+  return total
+}
+
+totalGenap = total(genap)
+console.log("Total genap:", totalGenap)
+
+totalGanjil = total(ganjil)
+console.log("Total ganjil:", totalGanjil)
+
 
 // Menghitung rata - rata
 let rataGenap = totalGenap / genap.length;
 let rataGanjil = totalGanjil / ganjil.length;
-let rata_rata = total / (genap.length + ganjil.length);
+let totalGenapGanjil = totalGenap + totalGanjil
+let rata_rata = totalGenapGanjil / (genap.length + ganjil.length);
 
 console.log("Rata-rata array genap", rataGenap);
 console.log("Rata-rata array ganjil", rataGanjil);
 console.log("Rata-rata kedua array", rata_rata);
 
-// Mencari perbandingan genap dan ganjil
-if (minGenap > minGanjil) {
-  console.log(
-    "Nilai minimum array genap lebih besar daripada nilai minimum array ganjil, yaitu",
-    minGenap + " > " + minGanjil
-  );
-} else {
-  console.log(
-    "Nilai minimum array genap lebih kecil daripada nilai minimum array ganjil, yaitu",
-    minGenap + " < " + minGanjil
-  );
+
+// Mencari perbandingan min dan max
+if (maxGanjil == maxGenap) {
+  console.log("Nilai max sama")
+} else if (maxGanjil > maxGenap) {
+  console.log("Max ganjil lebih besar")
+} else if (maxGanjil < maxGenap) {
+  console.log("Max genap lebih besar")
 }
 
-if (maxGenap > maxGanjil) {
-  console.log(
-    "Nilai maksimal array genap lebih besar daripada nilai maksimal array ganjil, yaitu",
-    maxGenap + " > " + maxGanjil
-  );
-} else {
-  console.log(
-    "Nilai maksimal array genap lebih kecil daripada nilai maksimal array ganjil, yaitu",
-    maxGenap + " < " + maxGanjil
-  );
+if (minGanjil == minGenap) {
+  console.log("Nilai min sama")
+} else if (minGanjil > minGenap) {
+  console.log("Min ganjil lebih besar")
+} else if (minGanjil < minGenap) {
+  console.log("Min genap lebih besar")
 }
 
-if (totalGenap === totalGanjil) {
-  console.log(
-    "Nilai total array genap dan ganjil sama, yaitu",
-    totalGenap + " = " + totalGanjil
-  );
-} else {
-  console.log(
-    "Total nilai array genap dan ganjil beda, yaitu",
-    totalGenap + " != " + totalGanjil
-  );
+
+// Perbandingan total
+if (totalGanjil == totalGenap) {
+  console.log("Nilai total sama")
+} else if (totalGanjil > totalGenap) {
+  console.log("Total ganjil lebih besar")
+} else if (totalGanjil < totalGenap) {
+  console.log("Total genap lebih besar")
 }
 
-if (rataGanjil > rataGenap) {
-  console.log(
-    "Rata-rata nilai array ganjil lebih besar daripada rata-rata nilai array genap, yaitu",
-    rataGanjil + " > " + rataGenap
-  );
-} else if (rataGenap > rataGanjil) {
-  console.log(
-    "Rata-rata nilai array genap lebih besar daripada rata-rata nilai array ganjil, yaitu",
-    rataGenap + " > " + rataGanjil
-  );
-} else {
-  console.log(
-    "Rata-rata nilai array ganjil sama dengan rata-rata nilai array genap, yaitu",
-    rataGanjil + " = " + rataGenap
-  );
+
+// Perbandingan rata - rata
+if (rataGanjil == rataGenap) {
+  console.log("Nilai rata-rata sama")
+} else if (rataGanjil > rataGenap) {
+  console.log("Rata-rata ganjil lebih besar")
+} else if (rataGanjil < rataGenap) {
+  console.log("Rata-rata genap lebih besar")
 }
